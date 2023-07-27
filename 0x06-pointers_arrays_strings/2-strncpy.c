4055b6d9a64946ec579de84d48e213d796bd0904
ochi234 <engineerjack254@gmail.com>
2023-07-27 05:37:10 -0700

    good

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -21,7 +21,7 @@ char *_strncpy(char *dest, char *src, int n)
 	dest[index] = src[index];
 
 	for (index = src_len; index < n; index++)
-	dest [index] = '\0';
+	dest[index] = '\0';
 
 	return (dest);
 }
