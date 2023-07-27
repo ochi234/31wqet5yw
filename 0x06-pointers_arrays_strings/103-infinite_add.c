163b1d68255173aea975995ab0fafe04f494a731
ochi234 <engineerjack254@gmail.com>
2023-07-27 10:18:48 -0700

    good

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,22 +8,22 @@
 
 void rev_string(char *n)
 {
-	int i = 0;
-	int j = 0;
-	char temp;
+int i = 0;
+int j = 0;
+char temp;
 
-	while (*(n + 1) != '\0')
-	{
-		i++;
-	}
-	i--;
+while (*(n + 1) != '\0')
+{
+i++;
+}
+i--;
 
-	for (j = 0; j < i; j++, i--)
-	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
-	}
+for (j = 0; j < i; j++, i--)
+{
+temp = *(n + j);
+*(n + j) = *(n + i);
+*(n + i) = temp;
+}
 }
 
 /**
@@ -36,38 +36,37 @@ void rev_string(char *n)
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, i = 0, j = 0, digit = 0;
-	int vall = 0, val2 = 0, temp_tot = 0;
-
-	while (*(n1 + i) != '\n')
-		i++;
-	while (*(n2 + j) != '\n')
-		j++;
-	i--;
-	j--;
-	if (j >= size_r || i >= size_r)
-		return (0);
-	while (j >= 0 || i >= 0 || overflow == 1)
-	{
-		if (i < 0)
-			vall = 0;
-		else
-			val2 = *(n2 + j) - '0';
-		temp_tot = vall + val2 + overflow;
-		if (temp_tot >= 10)
-			overflow = 1;
-		else
-			overflow = 0;
-		 if (digit >= (size_r - 1))
-			return (0);
-		*(r + digit) = (temp_tot % 10) + '0';
-		digit++;
-		j--;
-		i--;
-	}
-	if (digit == size_r)
-		return (0);
-	*(r + digit) = '\0';
-	rev_string(r);
-	return (r);
+int overflow = 0, i = 0, j = 0, digit = 0;
+int vall = 0, val2 = 0, temp_tot = 0;
+while (*(n1 + i) != '\0')
+i++;
+while (*(n2 + j) != '\0')
+j++;
+i--;
+j--;
+if (j >= size_r || i >= size_r)
+return (0);
+while (j >= 0 || i >= 0 || overflow == 1)
+{
+if (i < 0)
+vall = 0;
+else
+val2 = *(n2 + j) -'0';
+temp_tot = vall + val2 + overflow;
+if (temp_tot >= 10)
+overflow = 1;
+else
+overflow = 0;
+if (digit >= (size_r - 1))
+return (0);
+*(r + digit) = (temp_tot % 10) + '0';
+digit++;
+j--;
+i--;
+}
+if (digit == size_r)
+return (0);
+*(r + digit) = '\0';
+rev_string(r);
+return (r);
 }
