ae9209941289a53acf930a90d9dbf436fc0372aa
ochi234 <engineerjack254@gmail.com>
2023-08-04 03:36:31 -0700

    write all program that prints all arguments

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print the the minimum number of coins to
+ *        make change for an amount of money.
+ * @argc: The number of argument supplied to the program.
+ * @argv: An array of the pointers to the arguiment.
+ *
+ * Return: If the no of argument is not exactly one -1
+ *         Otherwise - 0.
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		cents--;
+	}
+
+	printf("%d\n", coins);
+
+	return (0);
+}
+
